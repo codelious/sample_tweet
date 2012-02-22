248f900d4e1935ce2f2d50aa0ab4e904b7b022fd
Mario Espinoza <mario@magrocode.com>
2012-02-22 00:04:55 -0300

    Borra index por defecto

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,17 +8,17 @@ describe "Paginas estaticas" do
   describe "Pagina Home" do
     
     it "debe tener el contenido 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_content('Sample App')
     end
     
     it "debe tener un h1 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('h1', :text => 'Sample App')
     end
 
     it "debe tener titulo 'Home'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('title',
                         :text => "#{base_title} | Home")
     end
@@ -27,16 +27,16 @@ describe "Paginas estaticas" do
   describe "pagina Help" do
     
     it "debe contener 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_content('Help')
     end
     it "debe tener el h1 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('h1', :text => 'Help')
     end
 
     it "debe tener el titulo 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('title',
                         :text => "#{base_title} | Help")
     end
@@ -45,16 +45,16 @@ describe "Paginas estaticas" do
   describe "pagina About" do
     
     it "debe contener 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_content('About Us')
     end
     it "debe tener el h1 'About'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('h1', :text => 'About Us')
     end
 
     it "debe tener el titulo 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('title',
                     :text => "#{base_title} | About Us")
     end
@@ -62,14 +62,15 @@ describe "Paginas estaticas" do
   
   describe "pagina Contact" do
     
-    it "debe contener 'Contact'" do
-      visit '/static_pages/contact'
-      page.should have_content('Contact')
-    end  
-    it "debe tener el titulo 'Contact'" do
-      visit '/static_pages/contact'
+    it "debe tener h1 'Contact'" do
+      visit contact_path
+      page.should have_selector('h1', text: 'Contact')
+    end
+
+    it "debe tener el title 'Contact'" do
+      visit contact_path
       page.should have_selector('title',
-                    :text => "#{base_title} | Contact")
+                    text: "Ruby on Rails Tutorial Sample App | Contact")
     end
   end
   
