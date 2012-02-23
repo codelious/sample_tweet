221cee834860036d6977f6047e03a71e4a6e571f
Mario Espinoza <mario@magrocode.com>
2012-02-23 12:50:50 -0300

    Finish user signup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -19,4 +19,28 @@ describe "UserPages" do
     it { should have_selector('title', text: 'Sign up') }
   end
   
+  describe "signup" do
+    
+    before { visit signup_path }
+    
+    describe "con informacion invalida" do
+      it "no debe crear un usuario" do
+        expect { click_button "Sign up" }.not_to change(User, :count)
+      end
+    end
+    
+    describe "con informacion valida" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Password confirmation", with: "foobar"
+      end
+      
+      it "debe crear un usuario" do
+        expect { click_button "Sign up" }.to change(User, :count).by(1)
+      end
+    end
+  end
+  
 end
